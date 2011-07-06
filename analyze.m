39100dfed3431cb9f8977803f88a65f4773855e5
Alberto Velandia <beto@yogadigital.net>
2011-07-06 18:34:42 -0500

    Insercion a db terminada y corte de mp3

--- analyze.m
+++ analyze.m
@@ -10,11 +10,11 @@
 %	Where XXXX =  Short name for the station corresponding to the value inserted in the db table Stations.
 
 
-% tks= myls(['/Users/albertovelandia/Music/Recordings/*.mp3']);
+tks= myls(['/Users/albertovelandia/Music/Recordings/*.mp3']);
 
 % 2. % Initialize the hash table database array 
 
-% clear_hashtable
+clear_hashtable
 
 % 3. Calculate the landmark hashes for each reference track and store
 %    it in the array, time depends on the Computers CPU and the encoding of the MP3, in Sonar the specs are:
@@ -23,7 +23,7 @@
 % 		Channels : Mono
 %		MP3 - Constant Bitrate
 
-% add_tracks(tks);
+add_tracks(tks);
 
 
 % 4. Connect to the MySQL Database
@@ -39,31 +39,48 @@ nads = length(ads);
 
 for i = 1:nads						    % For each ad on the folder
 	[dt,srt] = mp3read(char(ads(1,i))); % Read the file and
+	
+	[duration, trash]  = size(dt);
 	R = match_query(dt,srt);    	    % Run the query
 	[nmatches,columnas]=size(R);
 	
-	for j= 1:nmatches				    									     % For every AD we need to find
+	for j = 1:nmatches				    									     % For every AD we need to find
+
 		url = tks( 1, R(j,1) );												     % The URL of the file
 		date = strrep(url, '.mp3', ''); 									     % Removing the extension of the string
 		date = strrep(date, '/Users/albertovelandia/Music/Recordings/', '');     % Removing the file location out of the equation
 		date = char(date);
 		mysql_station = date(1:4);											     % We extract the short name of the radio Station
 		date = strrep(date, 'CARA ', '');										 % And obtaining the time of the start of the recording
-		queryString = sprintf('SELECT id FROM Stations WHERE short_name = "%d"', mysql_station);			 								     
-		mysql_station_id = mysql(QueryString); % With the short name of the radio station we locate its id.
-		disp(mysql_station_id);
-		startR = datevec(date, 'YYYY-MM-DD HH-mm-SS'); 						     % Converting the date string in to a variable that can be handled by matlab as a date
-		startA = addtodate(datenum(startR), round(R(i,3)*0.032), 'second' );     % Adding the delay found by the algorithm in steps of 32 ms
-		mysql_time = datestr( startA , 'YYYY-MM-DD HH:mm:SS' );				     % Converting that into a format that can be saved as a datetime variable in MySQL
+		queryString = sprintf('SELECT id FROM Stations WHERE short_name = "%s"', mysql_station);	 								     
+		mysql_station_id = mysql(queryString); 									 % With the short name of the radio station we locate its id.
+		startR = datenum(date, 'yyyy-mm-dd HH-MM-SS');
+		startA = addtodate(startR, round( R(j,3)*0.032 ), 'second' );     		 % Adding the delay found by the algorithm in steps of 32 ms
+		mysql_time = datestr(startA , 'yyyy-mm-dd HH:MM:SS' );				     % Converting that into a format that can be saved as a datetime variable in MySQL
 		mysql_landmarks = R(j,2);											     % Obtaining the number of landmarks that the algorithm found
 		mysql_ad_id = strrep( char(ads(1,i)) , '.mp3', '');					     % Extracting the id of the ad we are processing
 		mysql_ad_id = strrep( mysql_ad_id, '/Users/albertovelandia/Music/Ads/', ''); % Cleaning the garbage out of the string and getting just the id
-
 		
 		% After getting the information for each report we proceed to insert it on the da
-		% mysql(' INSERT INTO Reports (created, time, landmarks, ad_id, station_id ) VALUES ( NOW() , "%03d", "%03d", "%03d", "%03d")' , mysql_time, mysql_landmarks, mysql_ad_id, mysql_station_id);
+		queryString = sprintf(' INSERT INTO Reports (created, time, landmarks, ad_id, station_id ) VALUES ( NOW() , "%s", %d, %s, %d);' , mysql_time, mysql_landmarks, mysql_ad_id, mysql_station_id);
+		insert_data = mysql( queryString );                                                  % Insert the report in to the database
 		
+		if( mysql_landmarks > 10)												% If we have a possitive match beyond the confirmation level
+			
+			delay = round ( R(j,3) *0.032 );
+			start_sample = ( delay * 11025 ) - 110250; 
+			end_sample =   ( delay * 11025 ) + duration + 11025;
+			url_file = tks( 1, R(j,1) );
+			[rec, srr] = mp3read( char(url_file) );
+			new_vave = rec( ( start_sample :  end_sample ), 1   );
+			last_id = mysql('SELECT LAST_INSERT_ID();');
+			filename = sprintf('/Users/albertovelandia/Music/Samples/%d.mp3', last_id );
+			mp3write(new_vave, 11025 , filename);	
+						
+		end
 		
 	end
 	
 end
+
+movefile('/Users/albertovelandia/Music/Recordings/*.mp3', '/Users/albertovelandia/Music/Processed'); % Moving all the processed files to a different folder so they are not analysed again in the future
